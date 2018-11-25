9483396c6d6adf003dc0c825e37ee34c5477cafc
muhammedzahidunal <muhammedzahid.unal@stu.fsm.edu.tr>
2018-11-25 20:05:30 +0300

    cw7

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
 const CACHE ='JS'
+const FILES = ['/ileriPrograming/','/ileriPrograming/CW7/','/ileriPrograming/CW7/CW7.html'
+,'/ileriPrograming/HW3/','/ileriPrograming/HW3/animation.html']
 function installCB(e) {
-  console.log('install oldu', e.request);
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
 }
 self.addEventListener('install', installCB)
+
+function cacheCB(e) { //cache first
+  let req = e.request
+  e.respondWith(
+    caches.match(req)
+    .then(r1 => r1 || fetch(req))
+    .catch(console.log)
+  )
+}
+self.addEventListener('fetch', cacheCB)
 
 function save(req, resp) {
   return caches.open(CACHE)
@@ -14,7 +30,6 @@ function save(req, resp) {
 }
 function fetchCB(e) { //fetch first
   let req = e.request
-  console.log('JS', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
