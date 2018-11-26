eacea9729fe61f6c9510d712cb9c5919f11e845b
muhammedzahidunal <muhammedzahid.unal@stu.fsm.edu.tr>
2018-11-26 13:04:25 +0300

    cw7

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE ='JS'
-const FILES = ['/ileriPrograming/','/ileriPrograming/data_analiseCW3/inspector.html','/ileriPrograming/CW7/CW7.html',
-'/ileriPrograming/ArrayDemo_m.zahidunal.html','/ileriPrograming/HW3/animation.html','/ileriProgramming/CW4',
-'/ileriProgramming/CW5/readFile.html','/ileriProgramming/CW6/Timing.html','/ileriProgramming/HW1Array.html'
-,'/ileriProgramming/HW2/Database.html']
+const CACHE ='mz'
+const FILES = ['/ileriProgramming/','/ileriProgramming/CW3/inspector.html','/ileriProgramming/CW7/CW7.html',
+'/ileriProgramming/CW2/ArrayDemo_m.zahidunal.html','/ileriProgramming/HW3/animation.html','/ileriProgramming/CW4/index.html',
+'/ileriProgramming/CW5/readFile.html','/ileriProgramming/CW6/Timing.html','/ileriProgramming/HW1/HW1Array.html'
+,'/ileriProgramming/HW2/Database.html'];
+
+
 function installCB(e) {
   e.waitUntil(
     caches.open(CACHE)
@@ -12,26 +14,17 @@ function installCB(e) {
 }
 self.addEventListener('install', installCB)
 
-function cacheCB(e) { //cache first
-  let req = e.request
-  e.respondWith(
-    caches.match(req)
-    .then(r1 => r1 || fetch(req))
-    .catch(console.log)
-  )
-}
-self.addEventListener('fetch', cacheCB)
-
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  }) 
+  })
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
   let req = e.request
+  console.log('JS', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
